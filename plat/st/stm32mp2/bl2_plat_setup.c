74246d617bc320c8c0ba1cffa9ebe2126948de82
Gonzalo Ruiz <Gonzalo.Ruiz@digi.com>
2024-07-25 15:35:40 +0200

    bl2_plat_setup: mp2: remove boardinfo from log
    'board_id' has been removed from 'bsec' since the BOARD_ID OTP is a
    custom STM implementation, producing the following message in the TF-A
    boot console:
    
        ERROR:   nvmem node board_id not found
    
    Remove the boardinfo log to avoid seeing this message.
    
    Signed-off-by: Gonzalo Ruiz <Gonzalo.Ruiz@digi.com>

--- plat/st/stm32mp2/bl2_plat_setup.c
+++ plat/st/stm32mp2/bl2_plat_setup.c
@@ -462,8 +462,6 @@ void bl2_el3_plat_arch_setup(void)
 		NOTICE("Model: %s\n", board_model);
 	}
 
-	stm32mp_print_boardinfo();
-
 	print_reset_reason();
 
 	if (boot_context->auth_status != BOOT_API_CTX_AUTH_NO) {
